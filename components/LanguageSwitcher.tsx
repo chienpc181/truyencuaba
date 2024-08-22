222775bf77562e551311bcde5afc4ca9b843f3d9
chien pham <chien@rangstrup.com>
2024-08-22 22:01:00 +0700

    add logo and icon

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -2,18 +2,18 @@
 
 import { useState, useEffect } from 'react';
 
-export default function LanguageSwitcher() {
-    const [language, setLanguage] = useState('vi');
+export default function LanguageSwitcher({language}: any) {
+    // const [language, setLanguage] = useState('vi');
 
-    useEffect(() => {
-        const savedLanguage = window.localStorage.getItem('language') || 'vi';
-        setLanguage(savedLanguage);
-    }, []);
+    // useEffect(() => {
+    //     const savedLanguage = window.localStorage.getItem('language') || 'vi';
+    //     setLanguage(savedLanguage);
+    // }, []);
 
     const handleLanguageToggle = () => {
         const newLanguage = language === 'en' ? 'vi' : 'en';
         window.localStorage.setItem('language', newLanguage);
-        setLanguage(newLanguage);
+        // setLanguage(newLanguage);
         window.location.reload();  // Force page reload to trigger useEffect
     };
 
@@ -30,3 +30,28 @@ export default function LanguageSwitcher() {
         </div>
     );
 }
+
+// 'use client';
+
+// export default function LanguageSwitcher() {
+//     const savedLanguage = window.localStorage.getItem('language') || 'vi';
+//     const handleLanguageToggle = () => {
+//         const newLanguage = savedLanguage === 'en' ? 'vi' : 'en';
+//         window.localStorage.setItem('language', newLanguage);
+//         window.location.reload(); 
+//     };
+
+//     return (
+//         <div className='flex items-center'>
+//             <span className='font-bold'>VIE</span>
+//             <input 
+//                 type="checkbox"  
+//                 className="toggle toggle-info mx-2" 
+//                 onChange={handleLanguageToggle} 
+//                 checked={savedLanguage === 'en'} 
+//             />
+//             <span className='font-bold'>ENG</span>
+//         </div>
+//     );
+// }
+
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,29 @@
+'use client'
 import React from 'react'
 import LanguageSwitcher from './LanguageSwitcher'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
+    const [language, setLanguage] = useState('');
+     useEffect(() => {
+        const savedLanguage = window.localStorage.getItem('language') || 'vi';
+        setLanguage(savedLanguage);
+    }, []);
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl text-orange-600">truyen-cua-ba</a>
+                {/* <a className="btn btn-ghost text-xl text-orange-600">truyen-cua-ba</a> */}
+                {/* <img src=''></img> */}
+                <Image
+                    src="/truyencuaba_logo.png"
+                    alt="logo"
+                    width={180}
+                    height={60}
+                />
             </div>
             <div className="flex-none">
-                <LanguageSwitcher></LanguageSwitcher>
+                {(language === 'vi' || language === 'en') && <LanguageSwitcher language={language}></LanguageSwitcher>}
                 {/* <ul className="menu menu-horizontal px-1">
                     <li><a>Link</a></li>
                     <li>
